33413da05bd8e57a622e0acfd27a1bcb831172d0
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-11 17:11:50 +0300

    Update EventsPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,14 +24,21 @@ import EditEventPage from './pages/EditEventPage';
 import EventDetailPage from './pages/EventDetailPage';
 import NewEventPage from './pages/NewEventPagePage';
 import EventsPage from './pages/EventsPage';
+import Root from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/events', element: <EventsPage /> },
-  { path: '/events/:eventId', element: <EventDetailPage /> },
-  { path: '/new', element: <NewEventPage /> },
-  { path: '/events/:eventId/edit', element: <EditEventPage /> },
-])
+  { 
+    path: '/', 
+    element: <Root />,
+    children: [
+      { path: '', element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'new', element: <NewEventPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router}/>;
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom';
+ 
+const DUMMY_EVENTS = [
+    {
+    id: 'e1',
+    title: 'Event 1'
+    },
+    {
+        id: 'e2',
+        title: 'Event 2'
+    },
+    {
+        id: 'e3',
+        title: 'Event 3'
+    },
+];
+
 const EventsPage = () => {
     return (
-        <h1>Events Page</h1>;
-    )
+        <>
+            <h1>Events Page</h1>;
+            <ul>
+                {DUMMY_EVENTS.map(event => <li key={event.id}>
+                    <Link to={event.id}>{event.title}</Link>
+                </li>)}
+            </ul>
+        </>
+    );
 }
 
 export default EventsPage;
