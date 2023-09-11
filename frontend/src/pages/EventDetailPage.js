fcbbca2e03e1aee874347e1f0c6fa8457b357761
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-11 17:25:25 +0300

    Update EventDetailPage

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,6 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 const EventDetailPage = () => {
+    const params = useParams();
+
     return (
-        <h1>Event Detail Page</h1>;
+        <>
+             <h1>Event Detail Page</h1>
+             <p>Event ID: {params.eventId}</p>
+        </>
     )
 }
 
