41330244729cd49fd51b5883371a72965fa476e6
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-13 17:10:55 +0300

    Update EventsPage.js

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,31 +1,37 @@
-import { Link } from 'react-router-dom';
- 
-const DUMMY_EVENTS = [
-    {
-    id: 'e1',
-    title: 'Event 1'
-    },
-    {
-        id: 'e2',
-        title: 'Event 2'
-    },
-    {
-        id: 'e3',
-        title: 'Event 3'
-    },
-];
+import { useEffect, useState } from 'react';
 
-const EventsPage = () => {
-    return (
-        <>
-            <h1>Events Page</h1>;
-            <ul>
-                {DUMMY_EVENTS.map(event => <li key={event.id}>
-                    <Link to={event.id}>{event.title}</Link>
-                </li>)}
-            </ul>
-        </>
-    );
+import EventsList from '../components/EventsList';
+
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
 }
 
 export default EventsPage;
