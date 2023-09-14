1c968a4fa13fc1c8c2497ad68537d549879f98e6
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-14 17:07:10 +0300

    Create loader function in EventDetailPage

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,14 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, json } from 'react-router-dom';
+import EventItem from '../components/EventItem';
 
 const EventDetailPage = () => {
-    const params = useParams();
 
     return (
-        <>
-             <h1>Event Detail Page</h1>
-             <p>Event ID: {params.eventId}</p>
-        </>
+        <EventItem event={} />
     )
 }
 
 export default EventDetailPage;
+
+export async const loader = ({request, params}) => {
+    const id = params.eventId;
+
+    const response = await fetch('http://localhost:8080/events/' + id);
+
+    if (!response.ok) {
+        throw json({'Could not fetch details for selected event.'});
+    } else {
+        return response;
+    }
+}
